31ed52a197547526ee5f4a2edcba013a9a37bbbf
Ahmedalagha1999 <89541565+Ahmedalagha1999@users.noreply.github.com>
2024-01-24 21:24:46 +0200

    Update task2.dart
    print all the prime numbers up to the use inputted number

--- task2.dart
+++ task2.dart
@@ -1,36 +1,30 @@
 import 'dart:io';
 
-bool isPrime(int number) {
-  if (number <= 1) {
-    // Numbers less than or equal to 1 are not prime
+void main() {
+  stdout.write('Enter a number: ');
+  int userInput = int.parse(stdin.readLineSync()!);
+
+  print('Prime numbers up to $userInput are:');
+  printPrimes(userInput);
+}
+
+void printPrimes(int limit) {
+  for (int number = 2; number <= limit; number++) {
+    if (isPrime(number)) {
+      print(number);
+    }
+  }
+}
+
+bool isPrime(int num) {
+  if (num < 2) {
     return false;
   }
-
-  for (int i = 2; i <= number / 2; i++) {
-    if (number % i == 0) {
-      // If the number is divisible by any number other than 1 and itself, it's not prime
+  for (int i = 2; i <= num / 2; i++) {
+    if (num % i == 0) {
       return false;
     }
   }
-
-  // If the loop completes without finding a divisor, the number is prime
   return true;
 }
 
-void main() {
-  // Example usage:
-  print("Enter the number");
-  int number;
-  try {
-    number = int.tryParse(stdin.readLineSync()!)!;
-  } catch (e) {
-    print('Invalid input. Please enter valid integers.');
-    return;
-  }
-
-  if (isPrime(number)) {
-    print('$number is a prime number.');
-  } else {
-    print('$number is not a prime number.');
-  }
-}
